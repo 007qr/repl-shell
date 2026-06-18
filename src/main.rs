dc403469ee40afa31ba497f3cac870c32ff0e847
007qr <aayushpatil558321@gmail.com>
2026-06-19 00:14:42 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {
-            let input = input.trim();
-            println!("{input}: command not found");
-        },
-        Err(err) => {
-            println!("error: {err}");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let input = input.trim();
+                println!("{input}: command not found");
+            },
+            Err(err) => {
+                println!("error: {err}");
+                break;
+            }
         }
     }
 }
