814effa34db2aeba07a069f0fdb8a4e5339a9bee
007qr <aayushpatil558321@gmail.com>
2026-06-25 11:12:29 +0530

    codecrafters submit [skip ci]

--- src/cmd/complete.rs
+++ src/cmd/complete.rs
@@ -8,8 +8,19 @@ impl ShellCmd for Complete {
     }
 
     fn run(&self, args: Vec<&str>, shell: &mut Shell) -> Result<CommandResult, ShellError> {
-        Ok(CommandResult::Silent) 
+        let mut args_iter = args.iter().peekable();
+        if let Some(arg) = args_iter.peek() {
+            if **arg == "-p" {
+                args_iter.next();
+
+                if let Some(value) = args_iter.peek() {
+                    return Ok(CommandResult::Streams { stdout: String::new(), stderr: format!("complete: {value}: no completion specification") });
+                } else {
+                    return Ok(CommandResult::Silent);
+                }
+            }
+        }
+
+        Ok(CommandResult::Silent)
     }
 }
-
-
